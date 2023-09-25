1137cfd7eae1319d49b73559e39fd64da87cf304
juliane.vmaran@gmail.com <juliane.vmaran@gmail.com>
2023-09-24 22:36:54 -0300

    feat: adiciona nova funcionalidade usuario

--- 006_Simple_CRUD/usuario/src/main/java/com/juhmaran/simplecrud/usuario/repository/UsuarioRepository.java
+++ 006_Simple_CRUD/usuario/src/main/java/com/juhmaran/simplecrud/usuario/repository/UsuarioRepository.java
@@ -1,2 +1,7 @@
-package com.juhmaran.simplecrud.usuario.repository;public interface UsuarioRepository {
+package com.juhmaran.simplecrud.usuario.repository;
+
+import com.juhmaran.simplecrud.usuario.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
--- 006_Simple_CRUD/usuario/src/main/java/com/juhmaran/simplecrud/usuario/rest/UsuarioController.java
+++ 006_Simple_CRUD/usuario/src/main/java/com/juhmaran/simplecrud/usuario/rest/UsuarioController.java
@@ -1,2 +1,44 @@
-package com.juhmaran.simplecrud.usuario.rest;public class UsuarioController {
+package com.juhmaran.simplecrud.usuario.rest;
+
+import com.juhmaran.simplecrud.usuario.domain.Usuario;
+import com.juhmaran.simplecrud.usuario.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> listUser() {
+        return usuarioRepository.findAll();
+    }
+
+    @PostMapping(value = "/register")
+    public Usuario addUser(@Valid @RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Usuario getUser(@PathVariable Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping(value = "/{id}/edit")
+    public Usuario editUser(@PathVariable Long id, @RequestBody Usuario usuario) {
+        usuario.setId(id);
+        return usuarioRepository.save(usuario);
+    }
+
+    @DeleteMapping(value = "/{id}/delete")
+    public void deleteUser(@PathVariable Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
 }
